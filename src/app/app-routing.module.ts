b13db319d7be995bfc2b9930f4dc03304bd0f695
avinash <avinashsaraogi@gmail.com>
2019-03-08 23:19:51 -0600

    Added Components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { HomeComponent } from './home/home.component';
+import { ViewTaskComponent } from './view-task/view-task.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'add-task', component: AddTaskComponent},
+  {path: 'view-task', component: ViewTaskComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { HomeComponent } from './home/home.component';
+import { ViewTaskComponent } from './view-task/view-task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTaskComponent,
+    HomeComponent,
+    ViewTaskComponent
   ],
   imports: [
     BrowserModule,
